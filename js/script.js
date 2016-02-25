c75a5fbb4a634256b1d3d0d16fdf7b65a1926918
Xinlai He <Xinlai_He@epam.com>
2016-02-25 11:06:11 +0800

    jquery manipulation

--- js/script.js
+++ js/script.js
@@ -8,39 +8,39 @@ $("#prepend button").click(function() {
 });
 
 $("#before button").click(function() {
-	
+	$("#before p").before("Yo! ");
 });
 
 $("#css1 button").click(function() {
-	
+    $("#css1 p").css("font-size","2em");
 });
 
 $("#css2 button").click(function() {
-	
+   $("#css2 p").css({"font-size":"2em","color":"red"});
 });
 
 $("#attr1 button").click(function() {
-	
+  console.log($("#attr1 p a").attr("href"));
 });
 
 $("#attr2 button").click(function() {
-
+  $("#attr2 p a").attr("href", "http://www.google.com");
 });
 
 $("#class1 button").click(function() {
-	
+  $("#class1 p").addClass("bg-primary");
 });
 
 $("#class2 button").click(function() {
-
+  alert($("#class2 p").hasClass("lead"));
 });
 
 $("#form1 button").click(function() {
-
+  console.log($("#form1 input").val());
 });
 
 $("#form2 button").click(function() {
-
+  $("#form2 input").val("hexinlai");
 });
 
 // handle the mouseover event here
